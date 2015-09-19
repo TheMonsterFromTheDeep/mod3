478ddc5ad456ba2800d4f50605c3305395609b56
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2015-09-19 14:26:34 -0600

    various optimizations

--- string.h
+++ string.h
@@ -7,7 +7,7 @@
 
 namespace mod3
 {
-	class string
+	typedef class string
 	{
 	private:
 		char* _text; //Stores the value of the string - not const because it needs to be able to be manipulated
@@ -20,35 +20,8 @@ namespace mod3
 			_length = 0; //The useable length of the string is zero
 		}
 
-		string(char* value) //Copy the data over so that the char* isn't overwritten by string manipulations
-		{
-			_length = 0;
-			while(*(value + _length) != '\0')
-			{
-				_length++;
-			}
-			_text = new char[_length + 1];
-			for(int i = 0; i < _length; i++)
-			{
-				*(_text + i) = *(value + i);
-			}
-			*(_text + _length) = '\0';
-		}
-
-		string(const char* value) //Initializing from a const char* requires copying the data over
-		{
-			_length = 0;
-			while(*(value + _length) != '\0') //Null terminator is end of string
-			{
-				_length++;
-			}
-			_text = new char[_length + 1];
-			for(int i = 0; i < _length; i++)
-			{
-				*(_text + i) = *(value + i);
-			}
-			*(_text + _length) = '\0'; //Add null terminator
-		}
+		string(char* value);
+		string(const char* value); //Initializing from a const char* requires copying the data over
 
 		string(char c) //Construct string from single char
 		{
@@ -64,139 +37,15 @@ namespace mod3
 			_length = s._length;
 		}	
 
-		void concatenate(char c)
-		{
-			char* tmp = _text;
-			_text = new char[_length + 2]; //Enough room for the new char and the null terminator
-			for(int i = 0; i < _length; i++)
-			{
-				*(_text + i) = *(tmp + i);
-			}
-			*(_text + _length) = c;
-			*(_text + _length + 1) = '\0';
+		void concatenate(char c);
+		void concatenate(const char* c); //Concatenate with a const char* 	
+		void concatenate(string s); //Concatenate with another string
 
-			delete[] tmp;
+		void insert(char c, int index);
+		void insert(const char* c, int index);
+		void insert(string s, int index);
 
-			_length += 1;
-		}
-
-		void concatenate(const char* c) //Concatenate with a const char* 
-		{
-			int c_length = 0;
-			while(*(c + c_length) != '\0') { c_length++; }
-
-			char* tmp = _text;
-			_text = new char[_length + c_length + 1];
-			for(int i = 0; i < _length; i++)
-			{
-				*(_text + i) = *(tmp + i);
-			}
-			for(int i = 0; i < c_length; i++)
-			{
-				*(_text + _length + i) = *(c + i);
-			}
-			*(_text + _length + c_length) = '\0';
-
-			delete[] tmp;
-
-			_length += c_length;
-		}
-		
-		void concatenate(string s) //Concatenate with another string
-		{
-			char* tmp = _text;
-			_text = new char[_length + s._length + 1];
-			for(int i = 0; i < _length; i++)
-			{
-				*(_text + i) = *(tmp + i);
-			}
-			for(int i = 0; i < s._length; i++)
-			{
-				*(_text + _length + i) = *(s._text + i);
-			}
-			*(_text + _length + s._length) = '\0';
-
-			delete[] tmp;
-
-			_length += s._length;
-		}
-
-		void insert(char c, int index)
-		{
-			if(index < 0 || index > _length) { throw new exception("Index out of bounds in string"); }
-			else
-			{
-				char* tmp = _text;
-				_text = new char[_length + 2];
-				for(int i = 0; i < index; i++)
-				{
-					*(_text + i) = *(tmp + i);
-				}
-				*(_text + index) = c;
-				for(int i = index; i < _length; i++)
-				{
-					*(_text + i + 1) = *(tmp + i);
-				}
-				*(_text + _length + 1) = '\0';
-				_length++;
-				delete[] tmp;
-			}
-		}
-
-		void insert(const char* c, int index)
-		{
-			if(index < 0 || index > _length) { throw new exception("Index out of bounds in string"); }
-			else
-			{
-				int c_length = 0;
-				while(*(c + c_length) != '\0') { c_length++; }
-
-				char* tmp = _text;
-				_text = new char[_length + c_length + 1];
-				for(int i = 0; i < index; i++)
-				{
-					*(_text + i) = *(tmp + i);
-				}
-				for(int i = 0; i < c_length; i++)
-				{
-					*(_text + index + i) = *(c + i);
-				}
-				for(int i = index; i < _length; i++)
-				{
-					*(_text + i + c_length) = *(tmp + i);
-				}
-				*(_text + _length + c_length) = '\0';
-				_length += c_length;
-				delete[] tmp;
-			}
-		}
-
-		void insert(string s, int index)
-		{
-			if(index < 0 || index > _length) { throw new exception("Index out of bounds in string"); }
-			else
-			{
-				char* tmp = _text;
-				_text = new char[_length + s._length + 1];
-				for(int i = 0; i < index; i++)
-				{
-					*(_text + i) = *(tmp + i);
-				}
-				for(int i = 0; i < s._length; i++)
-				{
-					*(_text + index + i) = *(s._text + i);
-				}
-				for(int i = index; i < _length; i++)
-				{
-					*(_text + i + s._length) = *(tmp + i);
-				}
-				*(_text + _length + s._length) = '\0';
-				_length += s._length;
-				delete[] tmp;
-			}
-		}
-
-		void replace(char find, char replace) 
+		void replace(char find, char replace)
 		{
 			for(int i = 0; i < _length; i++)
 			{
@@ -213,73 +62,238 @@ namespace mod3
 		void operator+=(char c) { concatenate(c); }
 		void operator+=(const char* c) { concatenate(c); }
 		void operator+=(string s) { concatenate(s); }
-		
-		bool equals(string s)
-		{
-			if(_length == s._length)
-			{
-				for(int i = 0; i < _length; i++)
-				{
-					if(*(_text + i) != *(s._text + i)) { return false; }
-				}
-				return true;
-			}
-			return false;
-		}		
+	
+		bool equals(const char* c) const;
+		bool equals(string s) const;
 
-		bool equals(const char* c)
-		{
-			int c_length = 0;
-			while(*(c + c_length) != '\0') { c_length++; }
-			if(_length == c_length)
-			{
-				for(int i = 0; i < _length; i++)
-				{
-					if(*(_text + i) != *(c + i)) { return false; }
-				}
-				return true;
-			}
-			return false;
-		}
+		bool operator==(const char* c) const { return equals(c); }
+		bool operator==(string s) const { return equals(s); }
 
-		bool operator==(const char* c) { return equals(c); }
-		bool operator==(string s) { return equals(s); }
-
-		char& operator[] (int index)
+		char& operator[] (int index) const
 		{
 			if(index < 0 || index >= _length) { throw new exception("Index out of bounds in string"); }
 			else { return *(_text + index); }
 		}
 
-		char* copy() //Return a copy of the pointer with the data
-		{
-			char* tmp = new char[_length + 1];
-			for(int i = 0; i <= _length; i++) //Copy data over including null terminator
-			{
-				*(tmp + i) = *(_text + i);
-			}
-			return tmp;
-		}
+		string copy() const; //Return a copy of the pointer with the data
+		char* text() const { return _text; } //Return the text pointer for whatever reason
 
-		char* text() //Return the text pointer for whatever reason
-		{
-			return _text;
-		}
-
-		char at(int index)
+		char at(int index) const
 		{
 			if(index < 0 || index >= _length) { throw new exception("Index out of bounds in string"); }
 			else { return *(_text + index); }
 		}
 		
-		int length() { return _length; } //Returns the length - the length is a private variable so it isn't screwed with		
+		int length() const { return _length; } //Returns the length - the length is a private variable so it isn't screwed with		
 
 		string operator+(string s) { string r(*this); r.concatenate(s); return r; }		
 		string operator+(const char* c) { string r(*this); r.concatenate(c); return r; }		
 		string operator+(char c) { string r(*this); r.concatenate(c); return r; }		
-	};
+	} string;
 
-	char* getSubstring(string base, int start, int end) //Returns char* so redundant conversion to string doesn't happen when char* wanted
+	string::string(char* value)
+	{
+		_length = 0;
+		while(*(value + _length) != '\0')
+		{
+			_length++;
+		}
+		_text = new char[_length + 1];
+		for(int i = 0; i < _length; i++)
+		{
+			*(_text + i) = *(value + i);
+		}
+		*(_text + _length) = '\0';
+	}
+
+	string::string(const char* value)
+	{
+		_length = 0;
+		while(*(value + _length) != '\0') //Null terminator is end of string
+		{
+			_length++;
+		}
+		_text = new char[_length + 1];
+		for(int i = 0; i < _length; i++)
+		{
+			*(_text + i) = *(value + i);
+		}
+		*(_text + _length) = '\0'; //Add null terminator
+	}
+
+	void string::concatenate(char c)
+	{
+		char* tmp = _text;
+		_text = new char[_length + 2]; //Enough room for the new char and the null terminator
+		for(int i = 0; i < _length; i++)
+		{
+			*(_text + i) = *(tmp + i);
+		}
+		*(_text + _length) = c;
+		*(_text + _length + 1) = '\0';
+
+		delete[] tmp;
+
+		_length += 1;
+	}
+
+	void string::concatenate(const char* c)
+	{
+		int c_length = 0;
+		while(*(c + c_length) != '\0') { c_length++; }
+
+		char* tmp = _text;
+		_text = new char[_length + c_length + 1];
+		for(int i = 0; i < _length; i++)
+		{
+			*(_text + i) = *(tmp + i);
+		}
+		for(int i = 0; i < c_length; i++)
+		{
+			*(_text + _length + i) = *(c + i);
+		}
+		*(_text + _length + c_length) = '\0';
+
+		delete[] tmp;
+
+		_length += c_length;
+	}
+
+	void string::concatenate(string s)
+	{
+		char* tmp = _text;
+		_text = new char[_length + s._length + 1];
+		for(int i = 0; i < _length; i++)
+		{
+			*(_text + i) = *(tmp + i);
+		}
+		for(int i = 0; i < s._length; i++)
+		{
+			*(_text + _length + i) = *(s._text + i);
+		}
+		*(_text + _length + s._length) = '\0';
+
+		delete[] tmp;
+
+		_length += s._length;
+	}
+
+	void string::insert(char c, int index)
+	{
+		if(index < 0 || index > _length) { throw new exception("Index out of bounds in string"); }
+		else
+		{
+			char* tmp = _text;
+			_text = new char[_length + 2];
+			for(int i = 0; i < index; i++)
+			{
+				*(_text + i) = *(tmp + i);
+			}
+			*(_text + index) = c;
+			for(int i = index; i < _length; i++)
+			{
+				*(_text + i + 1) = *(tmp + i);
+			}
+			*(_text + _length + 1) = '\0';
+			_length++;
+			delete[] tmp;
+		}
+	}
+
+	void string::insert(const char* c, int index)
+	{
+		if(index < 0 || index > _length) { throw new exception("Index out of bounds in string"); }
+		else
+		{
+			int c_length = 0;
+			while(*(c + c_length) != '\0') { c_length++; }
+
+			char* tmp = _text;
+			_text = new char[_length + c_length + 1];
+			for(int i = 0; i < index; i++)
+			{
+				*(_text + i) = *(tmp + i);
+			}
+			for(int i = 0; i < c_length; i++)
+			{
+				*(_text + index + i) = *(c + i);
+			}
+			for(int i = index; i < _length; i++)
+			{
+				*(_text + i + c_length) = *(tmp + i);
+			}
+			*(_text + _length + c_length) = '\0';
+			_length += c_length;
+			delete[] tmp;
+		}
+	}
+
+	void string::insert(string s, int index)
+	{
+		if(index < 0 || index > _length) { throw new exception("Index out of bounds in string"); }
+		else
+		{
+			char* tmp = _text;
+			_text = new char[_length + s._length + 1];
+			for(int i = 0; i < index; i++)
+			{
+				*(_text + i) = *(tmp + i);
+			}
+			for(int i = 0; i < s._length; i++)
+			{
+				*(_text + index + i) = *(s._text + i);
+			}
+			for(int i = index; i < _length; i++)
+			{
+				*(_text + i + s._length) = *(tmp + i);
+			}
+			*(_text + _length + s._length) = '\0';
+			_length += s._length;
+			delete[] tmp;
+		}
+	}
+
+	bool string::equals(string s) const
+	{
+		if(_length == s._length)
+		{
+			for(int i = 0; i < _length; i++)
+			{
+				if(*(_text + i) != *(s._text + i)) { return false; }
+			}
+			return true;
+		}
+		return false;
+	}
+
+	bool string::equals(const char* c) const
+	{
+		int c_length = 0;
+		while(*(c + c_length) != '\0') { c_length++; }
+		if(_length == c_length)
+		{
+			for(int i = 0; i < _length; i++)
+			{
+				if(*(_text + i) != *(c + i)) { return false; }
+			}
+			return true;
+		}
+		return false;
+	}
+
+	string string::copy() const
+	{
+		string tmp;
+		tmp._text = new char[_length + 1];
+		tmp._length = _length;
+		for(int i = 0; i <= _length; i++) //Copy data over including null terminator
+		{
+			*(tmp._text + i) = *(_text + i);
+		}
+		return tmp;
+	}
+
+	char* substring(const string& base, int start, int end) //Returns char* so redundant conversion to string doesn't happen when char* wanted
 	{
 		if((start < 0 || start >= base.length()) || (end < 0 || end >= base.length())) { throw new exception("Index out of bounds in string"); }
 		else 
